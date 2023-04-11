70b95bbec3ff8186c168eafae1ed730c9dd9147c
Chinonso Mogo <mogononso@gmail.com>
2023-04-11 16:06:23 -0230

    getting wordset from DB into state variable

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,20 +3,35 @@ import './App.css';
 import Board from './component/Board';
 import Keyboard from './component/Keyboard';
 import { createContext, useEffect, useState } from 'react';
-import { boardDefault, generateWordSet } from './Words'
+import { boardDefault, generateWordSet } from './Words';
+import axios from 'axios';
 
 export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0})
+  const [wordSet, setWordSet] = useState(new Set())
 
   const correctWord = "RIGHT";
 
   useEffect(() => {
-    generateWordSet().then((words) =>{
-      console.log(words);
-    })
+    const loadWords = async () => {
+      const wordBankArr = [];
+      const response = await axios.get(`/api`);
+      const booksData = response.data;
+      for await(const item of booksData) {
+        wordBankArr.push(item['aback'])
+        }
+      
+        let newWordSet = new Set(wordBankArr)
+        setWordSet(newWordSet)
+        
+      }
+    loadWords();
+    // generateWordSet().then((words) => {
+    //   setWordSet(words.wordSet)
+    // })
   }, [])
 
   const onSelectLetter = (keyVal) => {
