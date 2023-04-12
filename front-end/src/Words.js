be922c96619260b19accac73926a9a399b5af02e
Chinonso Mogo <mogononso@gmail.com>
2023-04-11 22:06:00 -0230

    added game over feature

--- front-end/src/Words.js
+++ front-end/src/Words.js
@@ -10,33 +10,4 @@ export const boardDefault = [
     ["", "", "", "", ""],
 ];
 
-// export const generateWordSet = async() => {
-//     let db;
-//     let wordBankArr= [];
 
-//     // const client = new MongoClient(process.env.MONGO_CONNECT);
-//     const client = new MongoClient('mongodb://127.0.0.1:27017');
-//     await client.connect();
-//     db = client.db('wordbank')
-//     let result = await db.collection("words").find({}).toArray()
-//     for await(const item of result) {
-//         wordBankArr.push(item['aback'])
-//     }
-
-//     let wordSet = new Set(wordBankArr)
-    
-//     return { wordSet }
-// }
-
-export const generateWordSet = async () => {
-    let wordSet;
-
-    await fetch(wordBank)
-        .then((response) => response.text())
-        .then((result) => {
-            const wordArr = result.split('\n');
-            wordSet = new Set(wordArr)
-        });
-
-        return { wordSet }
-}
--- front-end/src/component/Letter.js
+++ front-end/src/component/Letter.js
@@ -13,7 +13,7 @@ function Letter({letterPos, attemptVal}) {
 
     const letter = board[attemptVal][letterPos];
 
-    const correct = correctWord[letterPos] === letter
+    const correct = correctWord.toUpperCase()[letterPos] === letter
     const almost = !correct && letter !== "" && correctWord.includes(letter);
 
     const letterState = currAttempt.attempt > attemptVal && 
